bc91761f47d59b90610492a5df860cb3e19061f0
Xinda Li <xinda.li@uwaterloo.ca>
2021-08-05 14:39:50 -0400

    Add noise clipping.

--- create_example.py
+++ create_example.py
@@ -26,7 +26,6 @@ c = conn.cursor()
 
 c.execute("CREATE TABLE table1 ( id INTEGER, val1 INTEGER, val2 INTEGER)")
 c.execute("CREATE TABLE table2 ( id INTEGER, prop INTEGER )")
-c.execute("CREATE TABLE sens ( id INTEGER, val1_sens INTEGER, val2_sens INTEGER)")
 
 A = np.random.choice(100, 10000)
 B = np.random.choice(100, 10000)
@@ -36,9 +35,6 @@ for a in A:
 
 for b in B:
     c.execute("INSERT INTO table2 VALUES ({},{})".format(b, np.random.randint(0, 2)))
-
-for i in range(100):
-    c.execute("INSERT INTO sens VALUES ({},{},{})".format(i, 99, 9))
 
 conn.commit()
 conn.close()
--- query.py
+++ query.py
@@ -3,18 +3,21 @@
 import argparse
 import sqlite3
 from numpy.random import laplace
+import numpy as np
 import pandas as pd
+
+# np.random.seed(0)
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("query_type", help="query type: 'sum' or 'count'")
-parser.add_argument("db_file", help="location of sqlite datbase")
+parser.add_argument("db_file", help="location of sqlite database")
 parser.add_argument("--eps", type=float, default=0.1, help="privacy parameter")
 parser.add_argument("--t1_name", default="table1", help="name of aggregated table")
 parser.add_argument("--t2_name", default="table2", help="name of second table")
 parser.add_argument("--t2_params", default=[], nargs="+", help="values of prop that filter second table")
+parser.add_argument("--sens", type=int, default=[], nargs="+", help="the sensitivity of each attribute")
 
 args = parser.parse_args()
-
 conn = sqlite3.connect(args.db_file)
 c = conn.cursor()
 
@@ -55,30 +58,28 @@ elif args.query_type == "count":
     print("Result of query: {:.2f}".format(res))
 
 elif args.query_type == "ldp_all":
-    c.execute(f"SELECT * FROM sens")
-    sens = c.fetchall()
-    sens_dict = {}
-    for id, val1_sens, val2_sens in sens:
-        sens_dict[id] = val1_sens, val2_sens
+    if len(args.sens) >= 2:
+        query = (f"SELECT DISTINCT {args.t1_name}.id, {args.t1_name}.val1, {args.t1_name}.val2 "
+                 f"FROM {args.t1_name},{args.t2_name} "
+                 f"WHERE {args.t1_name}.id = {args.t2_name}.id ")
+        if args.t2_params:
+            query += (f"AND {args.t2_name}.prop IN "
+                      f"({', '.join(args.t2_params)})")
 
-    query = (f"SELECT DISTINCT {args.t1_name}.id, {args.t1_name}.val1, {args.t1_name}.val2 "
-             f"FROM {args.t1_name},{args.t2_name} "
-             f"WHERE {args.t1_name}.id = {args.t2_name}.id ")
-    if args.t2_params:
-        query += (f"AND {args.t2_name}.prop IN "
-                  f"({', '.join(args.t2_params)})")
+        c.execute(query)
+        res = c.fetchall()
 
-    c.execute(query)
-    res = c.fetchall()
-    res = res if res else 0
-    for i, entry in enumerate(res):
-        new_entry = (entry[1] + laplace(0, sens_dict[entry[0]][0] / args.eps),
-                     laplace(0, sens_dict[entry[0]][1] / args.eps))
-        res[i] = new_entry
-    df = pd.DataFrame(res, columns=['val1', 'val2'])
-    print("Result of query (first 5 lines): ")
-    print(df.head())
-    df.to_csv('result.csv', index=False)
+        for i, entry in enumerate(res):
+            noise_1 = int(np.floor(laplace(0, args.sens[0] / args.eps)))  # clip the noise
+            noise_2 = int(np.floor(laplace(0, args.sens[1] / args.eps)))
+            new_entry = (entry[1] + noise_1, entry[2] + noise_2)
+            res[i] = new_entry
+        df = pd.DataFrame(res, columns=['val1', 'val2'])
+        print("Result of query (first 5 lines): ")
+        print(df.head())
+        df.to_csv('result.csv', index=False)
+    else:
+        print('Require a sensitivity for every attribute.')
 
 conn.commit()
 conn.close()
